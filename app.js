d312cba65bf3c3c053b7a7952befa9b3242eac64
borodos <stepllerrus78@gmail.com>
2022-03-30 03:09:03 +0300

    sssddd

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // - Объявление переменных, с которыми будем работать
-const allTasks = [];
+const allTasks = JSON.parse(localStorage.getItem("allTasks")) || [];
+completeTask = false;
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
@@ -7,21 +8,35 @@ const filterOption = document.querySelector("#filter-todo");
 
 // - Вешаем прослушку событий на элементы
 todoButton.addEventListener("click", addToDo);
-todoList.addEventListener("click", deleteCheck);
+todoList.addEventListener("click", checkTask);
 filterOption.addEventListener("click", filterToDo);
 
-async function Func() {
-	let response = await fetch("https://swapi.dev/api/people/4");
-	const json = await response.json();
-	return json;
-}
+/** localStorage и sessionStorage - хранилища браузера
+ * 
+// localStorage - используется, когда нужны постоянные изменения (переход на новую влкадку с тем же адресом, обновление страницы). Хранит значения в виде string
+// sessionStorage - используется, когда данные должны быть доступны только на время работы приложения (при обновлении страницы, перезапуске компа, данные должны удалиться)
+// Данные хранятся в виде "ключ, значение" - ('key', value)
 
-// Func();
+// ----------------- Хранилище -----------------
+// Так как в хранилище данные должны хранится в формате string, то данные нужно преобразовать => JSON.stringify(...);
+localStorage.setItem("allTasks", JSON.stringify(allTasks));
 
-window.onload = async function () {
-	const response = await Func();
-	localStorage.setItem("allTasks", response.name);
-};
+//  Далее при получении данных их нужно спарсить JSON.parse(localStorage.getItem('...'));
+//  Метод JSON.parse() разбирает строку JSON
+const tasks = JSON.parse(localStorage.getItem("allTasks"));
+console.log(tasks);
+ */
+
+// async function Func() {
+// 	let response = await fetch("https://swapi.dev/api/people/4");
+// 	const json = await response.json();
+// 	return json;
+// }
+
+// window.onload = async function () {
+// 	const response = await Func();
+// 	localStorage.setItem("allTasks", response);
+// };
 
 /**
  * - Либо так:
@@ -40,7 +55,11 @@ function addToDo(event) {
 	newTodo.innerText = todoInput.value;
 	newTodo.classList.add("todo-item");
 	todoDiv.append(newTodo);
-	allTasks.push(todoInput.value);
+	allTasks.push({
+		text: todoInput.value,
+		isCheck: false,
+	});
+	localStorage.setItem("allTasks", JSON.stringify(allTasks));
 
 	// - Обнуляем input и возвращаем на него фокус
 	todoInput.value = "";
@@ -48,23 +67,39 @@ function addToDo(event) {
 
 	// - Создаем кнопку выполнения задачи
 	const completedButton = document.createElement("button");
+	completedButton;
 	completedButton.innerHTML = '<i class="fa-solid fa-check"></i>';
 	completedButton.classList.add("complete-btn");
 	todoDiv.append(completedButton);
-
 	// - Создаем кнопку удаления задачи
 	const trashButton = document.createElement("button");
 	trashButton.innerHTML = '<i class="fa-solid fa-trash"></i>'; // - Устанавливаем иконку на кнопку
 	trashButton.classList.add("trash-btn"); // - Добавляем класс для button
 	todoDiv.append(trashButton);
 
+	// При переборе массива объектов, на каждый элемент по ОТДЕЛЬНОСТИ вешается событие клика кнопки
+	allTasks.map((value, index, array) => {
+		completedButton.onclick = function () {
+			// При первой итерации цикла - index = 0 - вешаем событие изменения isCheck
+			// При второй итерации цикла - index = 1 - вешаем событие изменения isCheck
+			// И т.д.
+			allTasks[index].isCheck = !allTasks[index].isCheck;
+			localStorage.setItem("allTasks", JSON.stringify(allTasks));
+		};
+		trashButton.onclick = function () {
+			allTasks.splice(index, 1);
+			localStorage.setItem("allTasks", JSON.stringify(allTasks));
+		};
+	});
+
 	// - Выводим в window
 	todoList.append(todoDiv);
 }
 
 // - Создание функции по удалению задачи и её выполнению
-function deleteCheck(e) {
-	const item = e.target; // - Свойство target является ссылкой на объект, который был инициатором события
+function checkTask(e) {
+	const item = e.target;
+	// item = e.target; // - Свойство target является ссылкой на объект, который был инициатором события
 	if (item.classList[0] === "trash-btn") {
 		const todo = item.parentElement; // - Родительский элемент - div с классом todo (создается на строке 17-19)
 		todo.classList.add("fall");
@@ -76,6 +111,7 @@ function deleteCheck(e) {
 		const todo = item.parentElement; // - Родительский элемент - div с классом todo (создается на строке 17-19)
 		todo.classList.toggle("completed");
 	}
+	// console.log(item.isCheck);
 }
 
 /**
